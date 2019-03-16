4127771af4b73f38678d849ccf1db5104cca99c3
deathmontain <jonatas.santos@aedu.com>
2019-03-16 20:30:54 -0300

    Configurando integração

---
+++ src/main/java/com/stefanini/api/CrudSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stefanini.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringProjectApplication.class, args);
+	}
+
+}
