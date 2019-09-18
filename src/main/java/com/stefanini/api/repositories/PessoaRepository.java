21bcb5ea118409828c81cee8a1aa5b781c683782
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-18 10:02:51 -0300

    Criado o repositorio pessoa.

--- src/main/java/com/stefanini/api/repositories/PessoaRepository.java
+++ src/main/java/com/stefanini/api/repositories/PessoaRepository.java
@@ -1,7 +1,6 @@
 package com.stefanini.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.stefanini.api.entities.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
