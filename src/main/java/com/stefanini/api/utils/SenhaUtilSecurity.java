eaa0acc68f675805bdc4a494b538b84ee673dcac
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-18 10:13:43 -0300

    Criado o metodo de validacao da senha.

--- src/main/java/com/stefanini/api/utils/SenhaUtilSecurity.java
+++ src/main/java/com/stefanini/api/utils/SenhaUtilSecurity.java
@@ -3,7 +3,7 @@ package com.stefanini.api.utils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SenhaUtilSecurity {
-	public static String gerarBcrypt(String senha) { 
+	public static String gerarBcrypt(String senha) {
 		if (senha == null) {
 			return senha;
 		}
@@ -13,6 +13,6 @@ public class SenhaUtilSecurity {
 	
 	public static boolean validaSenha(String senha, String senhaEncoded) {
 		BCryptPasswordEncoder validaSenha = new BCryptPasswordEncoder();
-		return validaSenha.matches(senha, senhaEncoded);
+		return validaSenha.matches(senha, senhaEncoded); 
 	}
 }
