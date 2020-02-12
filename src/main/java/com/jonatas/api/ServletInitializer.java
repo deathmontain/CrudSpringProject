10f85c2ba2fd61c7728b6592d25c1c943283f01e
Jônatas Santos <jonatas.santos@aedu.com>
2020-02-12 10:23:38 -0300

    Adicionado a função de busca por pessoa no repository.

--- src/main/java/com/jonatas/api/ServletInitializer.java
+++ src/main/java/com/jonatas/api/ServletInitializer.java
@@ -1,13 +1,25 @@
 package com.jonatas.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 public class ServletInitializer extends SpringBootServletInitializer {
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CrudSpringProjectApplication.class);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return ards -> {
+			System.out.println("Quantidade de elementos por página: " + this.qtdPorPagina);
+		};
 	}
 
 }
--- src/main/java/com/jonatas/api/repositories/PessoaRepository.java
+++ src/main/java/com/jonatas/api/repositories/PessoaRepository.java
@@ -4,5 +4,7 @@ import com.jonatas.api.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+
+    Pessoa findByRgPessoa(String rgPessoa);
 	
 }
